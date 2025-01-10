0a0900ae7e6f262c974543de3b62ab99553dcaf5
unknown <rodrigoavelar307@gmail.com>
2025-01-10 03:35:57 -0600

    Added_dash

--- nuevo-proyecto-de-juego/Escenas/navesita.gd
+++ nuevo-proyecto-de-juego/Escenas/navesita.gd
@@ -1,32 +1,28 @@
 class_name navesita
 extends CharacterBody2D
 
+###############################################################################
+#Variables Modificables
 @export var speed = 300
-@export var dash_accel = 100
+@export var dash_accel = 10
 @export var bullet_speed = 1000
 @export var fire_rate = 0.2
 
-
+###############################################################################
+#Variables externas
 @onready var PropulsorAnim = $AnimationPlayer
 @onready var AnimTree : AnimationTree = $AnimationTree
-#@onready var Shield_anim = $Shield
 var ESCENABALA = preload("res://Escenas/bullet.tscn")
 
+###############################################################################
+#Variables internas
+var dashDirection = Vector2.ZERO
+var dash_vector = Vector2.LEFT
+
+###############################################################################
+#Funciones principales
 func _ready() -> void:
 	AnimTree.active = true
-
-func bulletShot():
-	var instanciabullet = ESCENABALA.instantiate()
-	instanciabullet.position = $bulletpoint.get_global_position()
-	instanciabullet.rotation_degrees = rotation_degrees
-	instanciabullet.apply_impulse(Vector2(bullet_speed,0).rotated(rotation))
-	get_tree().get_root().add_child(instanciabullet)
-	
-
-func MouseFollow():
-	look_at(get_global_mouse_position())
-	pass
-
 
 func _process(delta: float) -> void:
 	MouseFollow()
@@ -39,9 +35,32 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float):
 	movement()
+	dash()
 	pass
+
+###############################################################################
+#Funciones generales
+#Funcion encargada de instanciar balas
+func bulletShot():
+	var instanciabullet = ESCENABALA.instantiate()
+	instanciabullet.position = $bulletpoint.get_global_position()
+	instanciabullet.rotation_degrees = rotation_degrees
+	instanciabullet.apply_impulse(Vector2(bullet_speed,0).rotated(rotation))
+	get_tree().get_root().add_child(instanciabullet)
 	
-	
+#Funcion encargada de la mecanica dash
+func dash():
+	if Input.is_action_just_pressed("Shift"):
+		velocity = velocity * dash_accel
+		move_and_slide()
+		print("dash")
+	pass
+#Funcion encargada de que la nave siga el cursor
+func MouseFollow():
+	look_at(get_global_mouse_position())
+	pass
+
+#Funcion encargada del movimiento del personaje
 func movement():
 	var directionX = Input.get_axis("Left","Right")
 	velocity.x = speed * directionX
@@ -50,7 +69,8 @@ func movement():
 	move_and_slide()
 	
 	pass
-	
+
+#Funcion encargada de las animaciones
 func updateAnimParameters():
 	if (velocity == Vector2.ZERO):
 		AnimTree["parameters/conditions/idle"] = true
@@ -61,6 +81,6 @@ func updateAnimParameters():
 		AnimTree["parameters/conditions/is_moving"] = true
 	
 	pass
-
+#Funcion encargada de la visibilidad del propusor Idle
 func PropulsorVisible():
 	$Propulsor.visible = !$Propulsor.visible
